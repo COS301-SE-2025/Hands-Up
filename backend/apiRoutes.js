840b6727e897f09d53ed514cc7eb52f703733fc2
Karabo <u23543087@tuks.co.za>
2025-05-22 15:05:15 +0200

    removed moved files

--- backend/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { learningProgress } from './controllers/dbController.js';
-
-const router = Router();
-
-router.post("/learning/progress", learningProgress);
-
-export default router;
