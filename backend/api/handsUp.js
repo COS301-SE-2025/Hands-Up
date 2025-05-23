885e827b8ab224ae17b7055c8d636a6bc6000712
Karabo <u23543087@tuks.co.za>
2025-05-23 19:03:09 +0200

    user learning progress successfully retrieved - error handling still to be added

--- backend/api/handsUp.js
+++ backend/api/handsUp.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Middleware
+app.use(express.json());
 app.use('/handsUPApi', apiRoutes);
 
 const server = app.listen(PORT, () => {
