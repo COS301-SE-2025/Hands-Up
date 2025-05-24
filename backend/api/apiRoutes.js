d24bef368a5559da2431731582640f26b1bd6e9a
Mutarisi <u23546094@tuks.co.za>
2025-05-24 21:12:52 +0200

    added route for signUp

--- backend/api/apiRoutes.js
+++ backend/api/apiRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { learningProgress } from './controllers/dbController.js';
+import { learningProgress,signUpUser } from './controllers/dbController.js';
 
 const router = Router();
 
 //These are out api routes
 router.post("/learning/progress", learningProgress);
+router.post("/auth/signup", signUpUser);  // Add this new route
 
 export default router;
