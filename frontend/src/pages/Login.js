2705b669c66b78074bd7a1e3ba2c63e3b1283e67
Mutarisi <u23546094@tuks.co.za>
2025-05-27 00:46:20 +0200

    removed unused import

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,6 @@
 
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useStatUpdater } from "../hooks/learningStatsUpdater";
 import '../styles/Login.css';
 import logo from '../logo.png';
 import {login} from'../utils/apiCalls.js'; 
