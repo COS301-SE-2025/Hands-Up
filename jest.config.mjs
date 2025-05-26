1f1f97a54f7dcd354ab600a0520464df175043c9
Karabo <u23543087@tuks.co.za>
2025-05-26 03:17:40 +0200

    added dependencies and coverage for testing

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,10 @@
 export default {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests/unit'],
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)', '**/?(*.)+(spec|test).mjs']
-};
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)', '**/?(*.)+(spec|test).mjs'],
+  collectCoverageFrom: [
+    "**/*.{js,jsx,mjs}",
+    "**/*.test.{js,jsx,mjs}"
+  ],
+  coverageDirectory: 'tests/coverage',
+}
