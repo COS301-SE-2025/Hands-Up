12d68b35963be58126a20ebfa17a7fb68b2b08a8
Karabo <u23543087@tuks.co.za>
2025-06-14 05:00:19 +0200

    added invalid sign, time delay, suggestions

--- ai_model/alphabetDetection.py
+++ ai_model/alphabetDetection.py
@@ -3,7 +3,10 @@ import numpy as np
 import tensorflow as tf
 import mediapipe as mp
 import pickle
+import time
+from collections import Counter
 
+# Load model and label encoder
 model = tf.keras.models.load_model('models/detectLettersModel.keras') 
 with open('models/labelEncoder.pickle', 'rb') as f:
     labelEncoder = pickle.load(f)
@@ -12,6 +15,12 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.3)
 
 cap = cv2.VideoCapture(0)
+
+PREDICTION_WINDOW = 5  
+CONFIDENCE_THRESHOLD = 0.8
+
+start_time = None
+predictions = []
 
 while True:
     ret, frame = cap.read()
@@ -33,14 +42,33 @@ while True:
                 dataAux.append(lm.y - yMin)
 
             input_data = np.array(dataAux, dtype=np.float32).reshape(1, 42, 1)
-
-            prediction = model.predict(input_data)
-            predictedIndex = np.argmax(prediction, axis=1)[0]
+            prediction = model.predict(input_data, verbose=0)[0]
+            confidence = np.max(prediction)
+            predictedIndex = np.argmax(prediction)
             predictedLabel = labelEncoder.inverse_transform([predictedIndex])[0]
 
-            mp.solutions.drawing_utils.draw_landmarks(frame, handLandmarks, mpHands.HAND_CONNECTIONS)
+            
+            if start_time is None:
+                start_time = time.time()
+                predictions = []
 
-            cv2.putText(frame, f'Prediction: {predictedLabel}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            predictions.append((predictedLabel, confidence))
+
+            mp.solutions.drawing_utils.draw_landmarks(frame, handLandmarks, mpHands.HAND_CONNECTIONS)
+            
+            labels = [label for label, conf in predictions if conf >= CONFIDENCE_THRESHOLD]
+            if labels:
+                final_prediction = Counter(labels).most_common(1)[0][0]
+                message = f'Prediction: {final_prediction}'
+            elif confidence < CONFIDENCE_THRESHOLD and confidence > 0.5:
+                message = f'Maybe {predictedLabel}, reposition hand and try again'
+            else:
+                message = f'Invalid sign, try again'
+
+            start_time = None
+            predictions = []
+
+            cv2.putText(frame, message, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
 
     cv2.imshow("ASL Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
