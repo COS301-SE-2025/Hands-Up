192240de547903c3287951522ac332292dd7e3c4
Thendo Mbedzi <u22585347@tuks.co.za>
2025-06-23 15:18:45 +0200

    fixed test codebase errors

--- backend/api/handsUp.js
+++ backend/api/handsUp.js
@@ -22,7 +22,7 @@ app.use(cors({
     origin: ['http://localhost:3000', 'https://localhost:3000'],
     credentials: true,           
 }));
-app.use()
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
