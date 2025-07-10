7482d99fd7a9f7264403646c8fee17549aaac53b
Karabo <u23543087@tuks.co.za>
2025-07-10 20:22:45 +0200

    base llm created - needs more training

---
+++ ai_model/llm/glossToEnglish.py
@@ -0,0 +1,13 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+model = T5ForConditionalGeneration.from_pretrained("./gloss2eng_model")
+tokenizer = T5Tokenizer.from_pretrained("./gloss2eng_model")
+
+def translate_gloss(gloss):
+    input_text = f"translate gloss to english: {gloss}"
+    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+    output_ids = model.generate(input_ids, max_length=32)
+    return tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+#Testing
+print(translate_gloss("MOTHER LOVE GIRL"))
