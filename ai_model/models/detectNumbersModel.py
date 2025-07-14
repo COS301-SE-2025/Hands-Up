71dae823d0ee8f036be5f487df3e15bc37615dc7
Karabo <u23543087@tuks.co.za>
2025-07-14 15:07:27 +0200

    numbers model saved

--- ai_model/models/detectNumbersModel.py
+++ ai_model/models/detectNumbersModel.py
@@ -3,11 +3,8 @@ import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
-from tensorflow.keras.callbacks import EarlyStopping
 
-early_stop = EarlyStopping(patience=2, restore_best_weights=True)
-
-dataDictTrain = pickle.load(open('../processed_data/trainData.pickle', 'rb'))
+dataDictTrain = pickle.load(open('../processed_data/numTrainData.pickle', 'rb'))
 
 cleanedData = []
 cleanedLabels = []
@@ -19,7 +16,7 @@ for i, item in enumerate(dataDictTrain['data']):
 xTrain = np.array(cleanedData)
 yTrainRaw = np.array(cleanedLabels)
 
-dataDictTest = pickle.load(open('../processed_data/testData.pickle', 'rb'))
+dataDictTest = pickle.load(open('../processed_data/numTestData.pickle', 'rb'))
 xTestRaw = np.array(dataDictTest['data'], dtype=np.float32)
 yTestRaw = np.array(dataDictTest['labels'])
 
@@ -31,26 +28,6 @@ labelEncoder.fit(yTrainRaw)
 
 yTrainEncoded = labelEncoder.transform(yTrainRaw)
 yTestEncoded = labelEncoder.transform(yTestRaw)
-
-#second data set for finetuning
-dataDictTrain2 = pickle.load(open('../processed_data/fineTuneData.pickle', 'rb'))
-
-cleanedData2 = []
-cleanedLabels2 = []
-for i, item in enumerate(dataDictTrain2['data']):
-    if isinstance(item, (np.ndarray, list)) and len(item) == 42:
-        cleanedData2.append(np.array(item, dtype=np.float32))
-        cleanedLabels2.append(dataDictTrain2['labels'][i])
-
-xTrain2 = np.stack(cleanedData2)
-yTrainRaw2= np.array(cleanedLabels2)
-
-xTrain2 = xTrain2.reshape(-1, 42, 1)
-
-labelEncoder2 = LabelEncoder()
-labelEncoder2.fit(yTrainRaw2)
-
-yTrainEncoded2 = labelEncoder2.transform(yTrainRaw2)
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv1D(32, kernel_size=3, activation='relu', input_shape=(42, 1)),
@@ -64,15 +41,6 @@ model = tf.keras.models.Sequential([
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(xTrain, yTrainEncoded, epochs=15, batch_size=32, validation_split=0.2)
 
-for layer in model.layers[:-2]:
-    layer.trainable = False
-
-model.compile(optimizer=tf.keras.optimizers.Adam(1e-5),loss='sparse_categorical_crossentropy',metrics=['accuracy'])
-
-yTrainEncoded2 = labelEncoder2.transform(yTrainRaw2)
-
-model.fit(xTrain2, yTrainEncoded2, epochs=10, batch_size=32, validation_split=0.2, callbacks = [early_stop])
-
 testLoss, testAccuracy = model.evaluate(xTest, yTestEncoded)
 print(f"\nTest accuracy: {testAccuracy * 100:.2f}%")
 
@@ -84,6 +52,6 @@ print("\nPredictions:")
 for i, pred in enumerate(predictedLabels):
     print(f"Image {i}: Predicted = {pred}, Actual = {yTestRaw[i]}")
 
-model.save("detectLettersModel.keras")
-with open("labelEncoder.pickle", "wb") as file:
+model.save("detectNumbersModel.keras")
+with open("numLabelEncoder.pickle", "wb") as file:
     pickle.dump(labelEncoder, file)
