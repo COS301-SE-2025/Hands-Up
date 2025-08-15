c0fcaa193dc09b4174b78885fbd0ed8d1cb98145
mutarisi <u23546094@tuks.co.za>
2025-08-15 13:21:46 +0200

    added file url path

--- backend/api/handsUp.js
+++ backend/api/handsUp.js
@@ -33,6 +33,8 @@ app.use(session({
     },
 }));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
