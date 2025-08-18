2bd83efb7a96b3bcc7e6110c4e45e705e604c583
Karabo <u23543087@tuks.co.za>
2025-08-18 09:45:02 +0200

    fixed build errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,94 +34,6 @@ function App() {
               <Route path="/login" element={
                 <ErrorBoundary fallback={<ErrorFallback errorName="Login" />}>
                   <Login />
-          <Routes>
-            <Route path="/" element={<Landing/>} />
-            <Route path="/landing" element={<Landing/>} />
-            <Route path="/login" element={
-              <ErrorBoundary fallback={<ErrorFallback errorName="Login" />}>
-                <Login />
-              </ErrorBoundary>
-            } />
-            <Route path="/signup" element={<ErrorBoundary fallback={<ErrorFallback errorName="Signup" />}>
-                <Signup />
-              </ErrorBoundary>
-            } />
-            <Route path="/reset-password/:token" element={<Login />} />
-
-            <Route path="/sign/:letter" element={
-              <ProtectedRoute>
-                <ErrorBoundary fallback={<ErrorFallback errorName="SignLearn" />}>
-                  <SignLearn />
-                </ErrorBoundary>
-              </ProtectedRoute>
-            } />
-
-            <Route path="/quiz/:category" element={
-              <ProtectedRoute>
-                <ErrorBoundary fallback={<ErrorFallback errorName="Quiz" />}>
-                  <SignQuiz />
-                </ErrorBoundary>
-              </ProtectedRoute>
-            } />
-            
-            <Route path="/quiz" element={<Navigate to="/quiz/alphabets" replace />} />
-            <Route path="/numbers-quiz" element={<Navigate to="/quiz/numbers" replace />} />
-            <Route path="/colours-quiz" element={<Navigate to="/quiz/colours" replace />} />
-            <Route path="/introduce-quiz" element={<Navigate to="/quiz/introduce" replace />} />
-            <Route path="/family-quiz" element={<Navigate to="/quiz/family" replace />} />
-            <Route path="/feelings-quiz" element={<Navigate to="/quiz/feelings" replace />} />
-            <Route path="/actions-quiz" element={<Navigate to="/quiz/actions" replace />} />
-            <Route path="/questions-quiz" element={<Navigate to="/quiz/questions" replace />} />
-            <Route path="/time-quiz" element={<Navigate to="/quiz/time" replace />} />
-            <Route path="/food-quiz" element={<Navigate to="/quiz/food" replace />} />
-            <Route path="/things-quiz" element={<Navigate to="/quiz/things" replace />} />
-            <Route path="/animals-quiz" element={<Navigate to="/quiz/animals" replace />} />
-            <Route path="/seasons-quiz" element={<Navigate to="/quiz/seasons" replace />} />
-            
-           <Route
-              path="/userProfile"
-              element={
-                <ProtectedRoute>
-                  <ErrorBoundary fallback={<ErrorFallback errorName="userProfile" />}>
-                  <Layout>
-                    <UserProfile />
-                  </Layout>
-                </ErrorBoundary>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/translator"
-              element={
-                <ProtectedRoute>
-                  <ErrorBoundary fallback={<ErrorFallback errorName="Translator" />}>
-                  <Layout>
-                    <Translator />
-                  </Layout>
-                </ErrorBoundary>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/learn"
-              element={
-                <ProtectedRoute>
-                  <ErrorBoundary fallback={<ErrorFallback errorName="Learn" />}>
-                  <Layout>
-                    <Learn />
-                  </Layout>
-                </ErrorBoundary>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/home"
-              element={
-                <ProtectedRoute>
-                   <ErrorBoundary fallback={<ErrorFallback errorName="Home" />}>
-                  <Layout>
-                    <Home />
-                  </Layout>
                 </ErrorBoundary>
               } />
               <Route path="/signup" element={<ErrorBoundary fallback={<ErrorFallback errorName="Signup" />}>
@@ -129,9 +41,95 @@ function App() {
                 </ErrorBoundary>
               } />
               <Route path="/reset-password/:token" element={<Login />} />
+
+              <Route path="/sign/:letter" element={
+                <ProtectedRoute>
+                  <ErrorBoundary fallback={<ErrorFallback errorName="SignLearn" />}>
+                    <SignLearn />
+                  </ErrorBoundary>
+                </ProtectedRoute>
+              } />
+
+              <Route path="/quiz/:category" element={
+                <ProtectedRoute>
+                  <ErrorBoundary fallback={<ErrorFallback errorName="Quiz" />}>
+                    <SignQuiz />
+                  </ErrorBoundary>
+                </ProtectedRoute>
+              } />
+              
+              <Route path="/quiz" element={<Navigate to="/quiz/alphabets" replace />} />
+              <Route path="/numbers-quiz" element={<Navigate to="/quiz/numbers" replace />} />
+              <Route path="/colours-quiz" element={<Navigate to="/quiz/colours" replace />} />
+              <Route path="/introduce-quiz" element={<Navigate to="/quiz/introduce" replace />} />
+              <Route path="/family-quiz" element={<Navigate to="/quiz/family" replace />} />
+              <Route path="/feelings-quiz" element={<Navigate to="/quiz/feelings" replace />} />
+              <Route path="/actions-quiz" element={<Navigate to="/quiz/actions" replace />} />
+              <Route path="/questions-quiz" element={<Navigate to="/quiz/questions" replace />} />
+              <Route path="/time-quiz" element={<Navigate to="/quiz/time" replace />} />
+              <Route path="/food-quiz" element={<Navigate to="/quiz/food" replace />} />
+              <Route path="/things-quiz" element={<Navigate to="/quiz/things" replace />} />
+              <Route path="/animals-quiz" element={<Navigate to="/quiz/animals" replace />} />
+              <Route path="/seasons-quiz" element={<Navigate to="/quiz/seasons" replace />} />
+              
+            <Route
+                path="/userProfile"
+                element={
+                  <ProtectedRoute>
+                    <ErrorBoundary fallback={<ErrorFallback errorName="userProfile" />}>
+                    <Layout>
+                      <UserProfile />
+                    </Layout>
+                  </ErrorBoundary>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/translator"
+                element={
+                  <ProtectedRoute>
+                    <ErrorBoundary fallback={<ErrorFallback errorName="Translator" />}>
+                    <Layout>
+                      <Translator />
+                    </Layout>
+                  </ErrorBoundary>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/learn"
+                element={
+                  <ProtectedRoute>
+                    <ErrorBoundary fallback={<ErrorFallback errorName="Learn" />}>
+                    <Layout>
+                      <Learn />
+                    </Layout>
+                  </ErrorBoundary>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <ErrorBoundary fallback={<ErrorFallback errorName="Home" />}>
+                    <Layout>
+                      <Home />
+                    </Layout>
+                  </ErrorBoundary>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/signup"
+              element={<ErrorBoundary fallback={<ErrorFallback errorName="Signup" />}>
+                  <Signup />
+                </ErrorBoundary>
+              } />
+            <Route path="/reset-password/:token" element={<Login />} />
               <Route path="/sign/:letter" element = {<SignLearn />} />
               <Route path="/quiz" element={<SignQuiz />} />
-            <Route
+              <Route
                 path="/userProfile"
                 element={
                   <ProtectedRoute>
@@ -204,7 +202,7 @@ function App() {
                   </ProtectedRoute>
                 }
               />
-              <Route path="*" element={<Navigate to="/login" />} />
+              <Route
                 path="/game"
                 element={
                   <ProtectedRoute>
