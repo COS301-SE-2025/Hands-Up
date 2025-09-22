8a0e9383d000ce16924db8b9b23808e981109ca7
mutarisi <u23546094@tuks.co.za>
2025-09-23 00:08:24 +0200

    fixing uvicorn

--- backend/api/handsUP.py
+++ backend/api/handsUP.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.apiRoutes import router as sign_router
@@ -7,12 +9,12 @@ app.include_router(sign_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://handsup.onrender.com"],  
+    allow_origins=["https://handsup.onrender.com"],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
