ab00a77e485df30ca6c17d0961ffa39b4a20a41e
mutarisi <u23546094@tuks.co.za>
2025-09-22 19:35:33 +0200

    specifying which url for origin

--- backend/api/handsUP.py
+++ backend/api/handsUP.py
@@ -7,7 +7,7 @@ app.include_router(sign_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://handsup.onrender.com"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
