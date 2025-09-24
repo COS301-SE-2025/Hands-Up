b2a6f6389c0e025e27cd8218f4823934670956ae
Karabo <u23543087@tuks.co.za>
2025-09-24 23:57:27 +0200

    re-trained numbers model

--- ai_model/pre_processing/processTrainData.py
+++ ai_model/pre_processing/processTrainData.py
@@ -11,12 +11,11 @@ mpDrawingStyles = mp.solutions.drawing_styles
 
 hands = mpHands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-DATADIR = '../numbers/nums_test'
+DATADIR = '../numbers/nums_train'
 
 data = []
 labels = []
 for dir in os.listdir(DATADIR):
-    #if dir.strip().upper() == 'C': #Starting with C because dataset too big
     for imgName in os.listdir(os.path.join(DATADIR, dir)):
         dataAux = []
 
@@ -31,34 +30,46 @@ for dir in os.listdir(DATADIR):
             print("Failed to load:", imgPath)
             continue  
 
-        img = cv2.imread(imgPath) # this iwwill read image and save to img variable
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
         results = hands.process(imgRGB)
-        if results.multi_hand_landmarks:
-            for handLandmarks in results.multi_hand_landmarks:
-                
-                for i in range(len(handLandmarks.landmark)):
-                    x = handLandmarks.landmark[i].x
-                    y = handLandmarks.landmark[i].y
 
+        if results.multi_hand_landmarks:
+            for hand_idx, handLandmarks in enumerate(results.multi_hand_landmarks):
+                # check handedness
+                handedness = results.multi_handedness[hand_idx].classification[0].label
+
+                # collect coordinates
+                x_ = []
+                y_ = []
+                for lm in handLandmarks.landmark:
+                    x = lm.x
+                    y = lm.y
                     x_.append(x)
                     y_.append(y)
 
-                for i in range(len(handLandmarks.landmark)):
-                    x = handLandmarks.landmark[i].x
-                    y = handLandmarks.landmark[i].y
-                    dataAux.append(x - min(x_))
-                    dataAux.append(y - min(y_))
+                # if left hand, flip horizontally across the midline
+                if handedness == "Left":
+                    x_ = [1 - x for x in x_]
 
-            data.append(dataAux)
-            labels.append(dir.strip().upper())
+                # normalize
+                for i in range(len(x_)):
+                    dataAux.append(x_[i] - min(x_))
+                    dataAux.append(y_[i] - min(y_))
 
-            # cv2.imshow("Hand Landmarks", img)
-            # cv2.waitKey(0)
+                data.append(dataAux)
+                labels.append(dir.strip().upper())
+
+                # Optional visualization
+                # mpDrawing.draw_landmarks(
+                #     img, handLandmarks, mpHands.HAND_CONNECTIONS,
+                #     mpDrawingStyles.get_default_hand_landmarks_style(),
+                #     mpDrawingStyles.get_default_hand_connections_style()
+                # )
+                # cv2.imshow("Hand", img)
+                # cv2.waitKey(0)
+
 print(labels)
 
-file = open('../processed_data/numTestData.pickle', 'wb')
+file = open('../processed_data/numTrainData.pickle', 'wb')
 pickle.dump({'data': data, 'labels': labels}, file)
 file.close()
-
